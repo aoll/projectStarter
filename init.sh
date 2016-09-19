e9d3f1e626105107c694fc4aa947f9e3f96ae153
aoll <agollivier@gmail.com>
2016-09-19 11:46:17 +0200

    init with unit_test

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#the value of the second entrie
 projecName=$1
 
+#create new file white the projectName
 mkdir $projecName;
 cd $projecName;
 
@@ -11,26 +13,39 @@ cd $projecName;
 #identification token to github username="aoll"
 # curl -i -u aoll -d '{"scopes": ["repo", "user"], "note": "getting-started"}' \
 #    https://api.github.com/authorizations
-#
+# curlGit=$(curl -i -u aoll -d '{"scopes": ["repo", "user"], "note": "getting-started"}' \
+#    https://api.github.com/authorizations)
+
+# echo $curlGit
 
 #CREATE NEW REPO WITH PROJECTNAME
-# curl -i -H 'Authorization: token exemple_token here' \
-#     -d '{ "name": "'$projecName'",  "auto_init": true,  "private": false }' \
+
+# curl -i -H 'Authorization: token 8c208076f71df2261aaa8ac22460a9dd82cbdf7c' \
+#     -d '{ "name": "'$projecName'",  "auto_init": false,  "private": false }' \
 #     https://api.github.com/user/repos
+
 
 mkdir src;
 cd src;
-cp /home/alex/c/main.c .;
+cp ../../projectStarter/main.c .;
 cd ..;
 mkdir inc;
 cd inc;
-cp /home/alex/c/project.h .;
+cp ../../projectStarter/project.h .;
 cd ..;
-cp /home/alex/c/Makefile .;
-cp -r /home/alex/c/libft .;
-touch README.md;
+git clone https://github.com/aoll/libft.git;
+git clone https://github.com/aoll/unit_test.git;
+# cd libft;
+# rm -rf .git;
+# cd ..;
+cp ../projectStarter/Makefile .;
+cp -r ../projectStarter/Documentation/.gitignore .;
+echo "# ''$projecName''" >> README.md
+
 # git init;
-# git add README.md;
+#
+# git add -A;
+#
 # git commit -m "first commit";
 # git remote add origin https://github.com/aoll/$projecName.git;
 # git push -u origin master;
